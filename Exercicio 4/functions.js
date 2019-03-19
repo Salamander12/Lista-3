a7fef37961c5bd436feee4a23fbfc92afa73b326
Victor Miguel <33434458+Salamander12@users.noreply.github.com>
2019-03-19 12:09:20 -0300

    Add files via upload

---
+++ Exercicio 4/functions.js
@@ -0,0 +1,8 @@
+function Vis_Inv(){
+	if (document.getElementById("P").style.visibility == "hidden")
+	{
+		document.getElementById("P").style.visibility = "visible"
+	} else {
+		document.getElementById("P").style.visibility = "hidden"
+	}
+}
