e96067f9839f02dfee4e3383bdcfd526659e8ef7
Victor Miguel <33434458+Salamander12@users.noreply.github.com>
2019-03-19 11:23:29 -0300

    Add files via upload

---
+++ Exercicio 2/functions.js
@@ -0,0 +1,13 @@
+function black(){
+	document.body.style.backgroundColor = "black";
+}
+function green(){
+	document.body.style.backgroundColor = "green";
+}
+
+function brown(){
+	document.body.style.backgroundColor = "brown";
+}
+function grey(){
+	document.body.style.backgroundColor = "grey";
+}
